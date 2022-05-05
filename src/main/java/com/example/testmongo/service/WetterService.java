6a4ffcc986885c969444f3c20ac2a307b53f164c
Mauritz Langer <mauritz@langer-net.de>
2022-05-05 17:02:26 +0200

    Schnelles Auslesen einer CSV-Datei und Speicherung der Daten in einer Datenbank

--- src/main/java/com/example/testmongo/service/WetterService.java
+++ src/main/java/com/example/testmongo/service/WetterService.java
@@ -9,6 +9,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class WetterService {
@@ -19,12 +22,18 @@ public class WetterService {
         this.wetterRepository = wetterRepository;
     }
 
-    public Wetter addWetter(Wetter wetter){
-        wetterRepository.save(wetter);
-        return wetter;
+    public List<Wetter> addWetterList(List<Wetter> wetterList){
+        wetterRepository.insert(wetterList);
+        return wetterList;
     }
 
+  public Wetter addWetter(Wetter wetter){
+    wetterRepository.save(wetter);
+    return wetter;
+  }
+
     public void readWetterFile(String filename){
+      System.out.println(LocalDateTime.now());
       String folder = "/Users/mauritz_langer/Downloads";
       File file = new File(folder + "/" + filename);
 
@@ -36,19 +45,33 @@ public class WetterService {
         in = new BufferedReader(new FileReader(file));
         String zeile = null;
         in.readLine();
+        int i = 0;
+        List<Wetter> wetterList = new ArrayList<>();
+        String[] filenameWithoutEnding;
+        if (filename.contains("_")) {
+          filenameWithoutEnding = filename.split("_");
+        } else {
+          filenameWithoutEnding = filename.split("\\.");
+        }
         while ((zeile = in.readLine()) != null) {
+          i++;
           String[] splitLine = zeile.split(",");
           Wetter wetter = new Wetter();
-          String[] filenameWithoutEnding = filename.split("_");
           wetter.setCountry(filenameWithoutEnding[0]);
           wetter.setTime(splitLine[0]);
-          System.out.println(splitLine[1]);
+          //System.out.println(splitLine[1]);
           wetter.setTemperature(Double.parseDouble(splitLine[1]));
-          addWetter(wetter);
+          wetterList.add(wetter);
+          if (i == 10000){
+            addWetterList(wetterList);
+            wetterList.clear();
+            i = 0;
+          }
         }
       } catch (IOException e) {
         e.printStackTrace();
       } finally {
+        System.out.println(LocalDateTime.now());
         if (in != null)
           try {
             in.close();
